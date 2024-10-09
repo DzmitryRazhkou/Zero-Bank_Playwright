5fa47bd632766dc30fac5b0e7a2040349d0631fe
Dzmitry Razhkou <86577413+DzmitryRazhkou@users.noreply.github.com>
2024-10-08 21:26:48 -0400

    Parallel Revised

--- playwright.config.js
+++ playwright.config.js
@@ -3,21 +3,20 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   testMatch: "**/*.spec.js",
-  // fullyParallel: true,
-  // forbidOnly: !!process.env.CI,
-  // retries: process.env.CI ? 2 : 0,
-  // workers: process.env.CI ? 1 : undefined,
-
-  reporter: "html",
+  retries: process.env.CI ? 2 : 0, // Retries in CI to handle flaky tests
+  workers: process.env.CI ? 1 : "100%", // Single worker in CI for stability, full capacity locally
+  fullyParallel: true, // Run tests in parallel by default
+  forbidOnly: !!process.env.CI, // Prevent accidental commits with .only in CI
+  reporter: "html", // Generate an HTML report
 
   use: {
-    browserName: "firefox",
-    headless: false,
-    screenshot: "only-on-failure",
-    video: "retain-on-failure",
-    ignoreHTTPSErrors: true,
-    trace: "on",
-    viewport: { width: 1920, height: 1080 },
-    baseURL: "http://zero.webappsecurity.com/index.html",
+    browserName: "firefox", // Set the browser for all tests
+    headless: true, // Run in headless mode for consistency in CI
+    screenshot: "only-on-failure", // Capture screenshots on failures for debugging
+    video: "retain-on-failure", // Retain video on failure for easier debugging
+    ignoreHTTPSErrors: true, // Ignore HTTPS errors
+    trace: "on-first-retry", // Enable tracing on the first retry for improved diagnostics
+    viewport: { width: 1920, height: 1080 }, // Set a common viewport size
+    baseURL: "http://zero.webappsecurity.com/index.html", // Set base URL
   },
 });
